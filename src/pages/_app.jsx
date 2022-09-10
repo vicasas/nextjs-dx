471b59cfc9175417404737928f4a2191f8d5b99b
Victor Casas <victor.casas@outlook.cl>
2022-09-10 14:35:21 -0400

    chore: Move to src folder

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
 import PropTypes from 'prop-types'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
